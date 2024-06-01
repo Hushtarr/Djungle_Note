18c32cb383373f12d11ba2f5f1d8ee3b98dbdedf
hushtar <HUXTAR.EZ@gmail.com>
2024-06-01 16:00:07 +0200

    Top 001 Updated

--- Code Hunting/Top_001.java
+++ Code Hunting/Top_001.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /*
 * You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n,
 * representing the number of elements in nums1 and nums2 respectively.
@@ -37,8 +39,12 @@ public class Top_001
         {
 
         Solution solution = new Solution();
-        //solution.merge();
-
+        int [] nums1 = {1,2,3,4,5,0,0,0};
+        int m=5;
+        int [] nums2={6,7,8};
+        int n=3;
+        solution.merge(nums1,m,nums2,n);
+            System.out.println(Arrays.toString(nums1));
         }
     public static class Solution
         {
@@ -48,24 +54,24 @@ public class Top_001
                 nums1 = [1,2,3,0,0,0], m = 3(valid number in second array)
                 nums2 = [2,5,6], n= 3(valid number in second array)
                 */
-                int LastIndex_First = m - 1; // Index of the last non-zero element in first array e:3 -> index:2
-                int LastIndex_Second = n - 1; // Index of the last non-zero element in second array e:6 -> index:2
-                int LastIndex_Merge = m + n - 1;// Index of the last  element in merged array e:3 -> index:2
+                int LastIndex_FirstAry = m - 1; // Index of the last non-zero element in first array e:3 -> index:2
+                int LastIndex_SecondAry = n - 1; // Index of the last non-zero element in second array e:6 -> index:2
+                int LastIndex_MergedAry = m + n - 1;// Index of the last  element in merged array e:3 -> index:2
 
-                while (LastIndex_Second >= 0)
-                    //In order to sort the merge arrays,
+                while (LastIndex_SecondAry >= 0)
+                    //To sort the merge arrays,
                     // they need to be compared with each other,
                     // so the index of any array returns to 0 to indicate the end of the merge.
                 {
                 // If the current element in nums1 is greater than the current element in nums2,
                 // Then copy the element in nums1 to the current last position of the merged array.
-                    if (LastIndex_First >= 0 && nums1[LastIndex_First] > nums2[LastIndex_Second])
+                    if (LastIndex_FirstAry >= 0 && nums1[LastIndex_FirstAry] > nums2[LastIndex_SecondAry])
                     {
-                    nums1[LastIndex_Merge--] = nums1[LastIndex_First--];
+                    nums1[LastIndex_MergedAry--] = nums1[LastIndex_FirstAry--];
                     }
                 else
                     {
-                    nums1[LastIndex_Merge--] = nums2[LastIndex_Second--];
+                    nums1[LastIndex_MergedAry--] = nums2[LastIndex_SecondAry--];
                     }
                 }
             }
